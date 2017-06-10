c7de3fa31c31967e101448a674352f20acd1a45c
vagrant <vagrant@homestead>
2017-06-10 23:31:25 +0000

    initial commit

---
+++ tests/Unit/BookmarkTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2017-06-10
+ * Time: 12:50 PM
+ */
+
+namespace Tests\Unit;
+
+
+use Tests\TestCase;
+
+class BookmarkTest extends TestCase
+{
+
+
+}

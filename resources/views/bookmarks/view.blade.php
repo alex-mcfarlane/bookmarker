e51dc8322115a39b341661df7f6ca9fe2e6df3ba
Alex McFarlane <alex.mcfarlane137@gmail.com>
2017-09-18 11:43:11 -0400

    display a single bookmark

--- resources/views/bookmarks/view.blade.php
+++ resources/views/bookmarks/view.blade.php
@@ -3,10 +3,24 @@
 @section('content')
     <div class="row">
         <div class="small-6 small-centered columns">
-            <div class="bookmark-container">
-                <h2><a href="{{$bookmark->url}}">{{$bookmark->title}}</a></h2>
+            <div class="bookmark bookmark-container">
+                <h2><a href="{{$bookmark->url}}" target="_blank">{{$bookmark->title}}</a></h2>
+
+                <div class="tagline">
+                    <p class="flex">Bookmarked from <a href="{{$bookmark->url}}" target="_blank">{{$bookmark->url}}</a></p>
+                    <a href="{{$bookmark->url}}" class="button tiny" target="_blank">Read More!</a>
+                </div>
 
                 <p>{{$bookmark->description}}</p>
+
+                <div class="actions-container">
+                    <a href="{{url('bookmarks/'.$bookmark->id.'/edit')}}">
+                        <i class="fa fa-pencil-square-o"></i>
+                    </a>
+                </div>
+                <div class="clearfix">
+
+                </div>
             </div>
         </div>
     </div>

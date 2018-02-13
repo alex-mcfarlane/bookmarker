7bb7530d3e55386d1d8a8ab5c68012c9f81381a9
Alex McFarlane <amcfarlane1251@gmail.com>
2018-02-13 16:03:42 -0500

    WIP

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permissions')->insert([
+            [
+                'id' => 1,
+                'name' => 'Create',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Read',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Update',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Delete',
+            ]
+        ]);
+    }
+}
